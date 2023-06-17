8f3f0948bb3d81b43447e56cfe04d278ae6c0744
Rostislav <ylrostik@mail.ru>
2023-06-17 16:24:05 +0300

    First commit

---
+++ huck_marks.py
@@ -0,0 +1,27 @@
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from datacenter.models import Schoolkid, Mark
+
+def fix_marks(student):
+    marks = Mark.objects.filter(schoolkid=student, points__in=[2, 3])
+    for mark in marks:
+        mark.points = 5
+        mark.save()
+
+def huck_murks(student_name):
+    try:
+        student = Schoolkid.objects.get(full_name__contains=student_name.title())
+    except MultipleObjectsReturned:
+        print(f'Найдено более одного ученика с таким именем - "{student_name}", введите полное имя: ФИО')
+    except ObjectDoesNotExist:
+        print('Ученика с таким именем нет в базе данных')
+    except (SyntaxError, AttributeError):
+        print('Ошибка ввода, необходимо ввести имя ученика в кавычках, например: "Петров Максим".')
+        return
+    except:
+        print('ошибка')
+    else:
+        fix_marks(student)
+
+
+if __name__ == "__main__":
+    huck_murks()
---
+++ update_chastisements.py
@@ -0,0 +1,92 @@
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from datacenter.models import Schoolkid, Chastisement, Subject, Commendation, Lesson
+from random import choice
+
+
+def remove_chastisement(studunt):
+    chastisements = Chastisement.objects.filter(schoolkid=studunt)
+    chastisements.delete()
+
+
+def create_commendation(student, subject, last_lesson):
+    examples_of_praise = [
+        'Молодец!',
+        'Отлично',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!'
+    ]
+    commendation = Commendation.objects.create(
+        text=choice(examples_of_praise),
+        created=(Lesson.objects.filter(group_letter=student.group_letter, subject=subject).order_by("-date").first()).date,
+        schoolkid=student,
+        subject=subject,
+        teacher=last_lesson.teacher
+    )
+
+
+def update_chastisements(student_name, lesson):
+    try:
+        student = Schoolkid.objects.get(full_name__contains=student_name.title())
+        subject = Subject.objects.filter(
+            title=lesson.capitalize(),
+            lesson__group_letter=student.group_letter,
+            year_of_study=student.year_of_study
+        ).first()
+        last_lesson = Lesson.objects.filter(
+            group_letter=student.group_letter,
+            subject=subject
+        ).order_by("-date").first()
+        if last_lesson is None:
+            print(f'Не найдено занятий по предмету - "{lesson}", проверьте ваш ввод')
+            return
+    except Schoolkid.DoesNotExist:
+        print(f'Ученика с именем - "{student_name}" нет в базе данных, исправьте имя ученика')
+        return
+    except Schoolkid.MultipleObjectsReturned:
+        print(f'Найдено более одного ученика с таким именем - {student_name}, введите полное имя: ФИО')
+        return
+    except Subject.DoesNotExist:
+        print(f'Предмета с названием - "{lesson}" нет в базе данных, проверьте ваш ввод')
+        return
+    except Subject.MultipleObjectsReturned:
+        print(f'Найдено более одного предмета с названием - "{lesson}", введите название предмета полностью')
+        return
+    except Lesson.DoesNotExist:
+        print(f'Не найдено занятий по предмету - "{lesson}" для ученика "{student.full_name}"')
+        return
+    except (SyntaxError, AttributeError):
+        print('''Ошибка ввода, необходимо ввести имя ученика в кавычках, например: "Петров Максим",
+и название предмета в кавычках, например: "Математика" через запятую.''')
+        return
+    remove_chastisement(student)
+    create_commendation(student, subject, last_lesson)
+
+
+if __name__ == "__main__":
+    update_chastisements()
